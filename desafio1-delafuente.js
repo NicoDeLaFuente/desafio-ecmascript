55ddfc07b5c861aebed6789261364cb6ac4ddf12
Nicolas De La Fuente <nicoolasdelafuente@gmail.com>
2023-06-16 10:28:56 -0300

    desafio-1

---
+++ desafio1-delafuente.js
@@ -0,0 +1,63 @@
+class ProductManager {
+    products;
+
+    static ultimoId = 0;
+
+    constructor (){
+        this.products = [];
+    }
+
+    getProducts(){
+        return this.products;
+    }
+
+    addProduct(title, description, price, thumbnail, code, stock) {
+        ProductManager.ultimoId++
+        const producto = {
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock,
+            id: ProductManager.ultimoId
+        }
+
+        // si hay un dato faltante, arroja error. 
+        if (!title, !description, !price, !thumbnail, !code, !stock) {
+            throw new Error("Por favor completar todos los campos necesarios");
+        }
+
+        const checkExistence = this.products.findIndex(product => product.code === code) 
+        
+        // si el codigo del producto coincide con uno existente, arroja error. 
+        if (checkExistence !== -1) {
+            throw new Error(`El producto ${title} ya existe`)
+        }
+
+        //pushea el producto en caso de pasar las verificaciones. 
+        this.products.push(producto);
+    }
+
+    getProductById(id){
+        const findId = this.products.findIndex(product => product.id === id)
+
+        if (findId === -1) {
+            throw new Error(`El id ${id} no ha sido encontrado`);
+        }
+
+        return this.products[findId];
+    }
+}
+
+
+
+const productos = new ProductManager()
+
+productos.addProduct("producto prueba", "este es un producto prueba", 200, "sin imagen", "abc123", 25);
+productos.addProduct("producto solar", "este es un producto prueba", 500, "sin imagen", "aas213", 5);
+productos.addProduct("producto luminico", "este es un producto prueba", 330, "sin imagen", "asd432", 10);
+
+
+console.log(productos.getProductById(6))
+
